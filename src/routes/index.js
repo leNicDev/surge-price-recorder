91996b227208c8acf1b21ebb3b61a5b3df320de7
leNicDev <germanelectronix@gmail.com>
2021-08-05 18:18:12 +0200

    Remove query from api response

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/surge', async (req, res) => {
     const data = await questDbClient.query('SELECT * FROM surge_price')
-    res.json(data)
+    res.json({ columns: data.columns, dataset: data.dataset, count: data.count })
 })
 
 router.get('/surgeusd', async (req, res) => {
