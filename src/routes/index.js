4f051a7b7e41c3c47f8feaaaa0b296cfe7c2c65e
leNicDev <germanelectronix@gmail.com>
2021-08-05 19:34:38 +0200

    Add more intervals

--- src/routes/index.js
+++ src/routes/index.js
@@ -24,10 +24,15 @@ function parseInterval(interval) {
 
     switch (interval.toLowerCase()) {
         case '1m': return '1m';
+        case '5m': return '5m';
+        case '15m': return '15m';
         case '1h': return '1h';
+        case '6h': return '6h';
+        case '12h': return '12h';
         case '1d': return '1d';
+        case '3d': return '3d';
         case '1w': return '1w';
-        case '1mo': return '30d';
+        case '1mo': return '4w';
         default: return '1d';
     }
 }
