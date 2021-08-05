fc04165a70b07aa9260b539c58203ee02114db73
leNicDev <germanelectronix@gmail.com>
2021-08-05 18:55:52 +0200

    Sample query results by 1d

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,12 @@ import { questDbClient } from '../app.js'
 const router = express.Router()
 
 router.get('/surge', async (req, res) => {
-    const data = await questDbClient.query('SELECT * FROM surge_price')
+    const data = await questDbClient.query('SELECT avg(surgePrice) AS price, timestamp FROM surge_price SAMPLE BY 1d ALIGN TO CALENDAR')
     res.json({ columns: data.columns, dataset: data.dataset, count: data.count })
 })
 
 router.get('/surgeusd', async (req, res) => {
-    const data = await questDbClient.query('SELECT * FROM surgeusd_price')
+    const data = await questDbClient.query('SELECT avg(surgeusdPrice) FROM surgeusd_price SAMPLE BY 1d ALIGN TO CALENDAR')
     res.json({ columns: data.columns, dataset: data.dataset, count: data.count })
 })
 
