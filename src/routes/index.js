0a2d180ef5ec9e7dab66930c4a12f6a4a8f6e25c
leNicDev <germanelectronix@gmail.com>
2021-08-06 23:59:25 +0200

    Add change/changePercentage api

--- src/routes/index.js
+++ src/routes/index.js
@@ -11,12 +11,40 @@ router.get('/surge', async (req, res) => {
     res.json({ columns: data.columns, dataset: data.dataset, count: data.count })
 })
 
+router.get('/surge/change', async (req, res) => {
+    const query = `SELECT (cast(last(surgePrice) AS DOUBLE) + -cast(first(surgePrice) AS DOUBLE)) AS change FROM surge_price WHERE timestamp >= dateadd('d', -1, now())`
+
+    const data = await questDbClient.query(query)
+    res.json({ change: data.dataset[0][0] })
+})
+
+router.get('/surge/changePercentage', async (req, res) => {
+    const query = `SELECT ((cast(last(surgePrice) AS DOUBLE) - cast(first(surgePrice) AS DOUBLE)) / cast(first(surgePrice) AS DOUBLE)) AS change FROM surge_price WHERE timestamp >= dateadd('d', -1, now())`
+
+    const data = await questDbClient.query(query)
+    res.json({ change: data.dataset[0][0] })
+})
+
 router.get('/surgeusd', async (req, res) => {
     const interval = parseInterval(req.query.interval)
     const query = `SELECT avg(surgeusdPrice) AS price, timestamp FROM surgeusd_price SAMPLE BY ${interval} ALIGN TO CALENDAR`
 
     const data = await questDbClient.query(query)
     res.json({ columns: data.columns, dataset: data.dataset, count: data.count })
+})
+
+router.get('/surgeusd/change', async (req, res) => {
+    const query = `SELECT (cast(last(surgeusdPrice) AS DOUBLE) + -cast(first(surgeusdPrice) AS DOUBLE)) AS change FROM surgeusd_price WHERE timestamp >= dateadd('d', -1, now())`
+
+    const data = await questDbClient.query(query)
+    res.json({ change: data.dataset[0][0] })
+})
+
+router.get('/surgeusd/changePercentage', async (req, res) => {
+    const query = `SELECT ((cast(last(surgeusdPrice) AS DOUBLE) - cast(first(surgeusdPrice) AS DOUBLE)) / cast(first(surgeusdPrice) AS DOUBLE)) AS change FROM surgeusd_price WHERE timestamp >= dateadd('d', -1, now())`
+
+    const data = await questDbClient.query(query)
+    res.json({ change: data.dataset[0][0] })
 })
 
 function parseInterval(interval) {
@@ -29,6 +57,7 @@ function parseInterval(interval) {
         case '1h': return '1h';
         case '6h': return '6h';
         case '12h': return '12h';
+        case '24h': return '1d';
         case '1d': return '1d';
         case '3d': return '3d';
         case '1w': return '1w';
